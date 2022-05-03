05f17e4e122ccb32cc3b4eb262c6977e76992eff
ihsan dihkan <dihkan@gmail.com>
2022-05-03 13:44:20 +0300

    dist eklendi

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,15 @@
 set -e
 
 # build
+npm run build
 
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
+git checkout -b main
 git add -A
 git commit -m 'deploy'
 
